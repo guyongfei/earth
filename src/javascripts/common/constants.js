8b1343e330c4fada1cb5602d61c8c7cd873deaf1
liuze <liuze@ibeesaas.com>
2018-07-18 13:48:02 +0800

    Fixed constants

--- src/javascripts/common/constants.js
+++ src/javascripts/common/constants.js
@@ -1,2 +1,2 @@
 // export const baseURL = 'http://qa.zhangrongxin.com/mars'; // 测试环境
-export const baseURL = 'http://www.tokenpie.io/mars'; // 正式环境
+export const baseURL = 'https://www.tokenpie.io/mars'; // 正式环境
