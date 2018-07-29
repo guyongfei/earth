bf9a5a874719289c8f62d400d9e8b8ea2c9b3e83
liuze <liuze@ibeesaas.com>
2018-07-29 22:02:24 +0800

    Fixed bug

--- src/javascripts/modules/baseform.js
+++ src/javascripts/modules/baseform.js
@@ -276,9 +276,7 @@ export default class Baseform {
         .then(res => {
           method.setCookie('logined', true);
           this.error($login, $.t('login.success'));
-          setTimeout(() => {
-            window.location.reload();
-          }, 300);
+          window.location.reload();
         })
         .catch(err => {
           this.error($login, err.message);
